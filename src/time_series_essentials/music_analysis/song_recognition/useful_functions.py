7eedff81de5822cd1a9e5d5a409b838213dba309
audentis-systems <contact@audentissystems.com>
2026-03-19 14:26:21 -0700

    Added documentation.

--- src/time_series_essentials/music_analysis/song_recognition/useful_functions.py
+++ src/time_series_essentials/music_analysis/song_recognition/useful_functions.py
@@ -6,6 +6,11 @@ import librosa
 #
 # Define a function that produces a chromagram for a track for a specific octave
 #
+# By performing this analysis one octave at a time and then
+# appending the results together (in a downstream functino),
+# we obtain a chromagram vector that covers every note on the
+# piano seperately per time step.
+#
 def process_octave(y, octave_number, sr = 22050, hop_length = 512 * 200, lowest_pitch = 16.35):  # get a more precise number for C0
     chromagram = librosa.feature.chroma_cens(
         y = y,
